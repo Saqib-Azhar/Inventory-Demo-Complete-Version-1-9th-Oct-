183120f968f53042442be406d7eb9f26bbd5fcf8
sazhar <saqibabdullahazhar@gmail.com>
2018-10-09 12:07:58 +0500

    First Stable Commit

---
+++ DemoInven/Controllers/OrdersController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DemoInven.Models;
+using Microsoft.AspNet.Identity;
+
+namespace DemoInven.Controllers
+{
+    [Authorize]
+    public class OrdersController : Controller
+    {
+        private DemoInvenEntities db = new DemoInvenEntities();
+
+        // GET: Orders
+        public ActionResult Index()
+        {
+            var orders = db.Orders.Include(o => o.AspNetUser);
+            return View(orders.ToList());
+        }
+        [Authorize]
+
+        // GET: Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var orderDetails = db.OrderDetails.Where(s=>s.OrderId == id);
+            if (orderDetails == null)
+            {
+                return HttpNotFound();
+            }
+            return View("","",orderDetails);
+        }
+        [Authorize]
+        // GET: Orders/Create
+        public ActionResult Create()
+        {
+            ViewBag.CashierId = new SelectList(db.AspNetUsers, "Id", "Email");
+            ViewBag.Products = db.Products.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult SubmitOrder(string CustomerName, int TotalPrice, List<OrderDetail> OrderDetails)
+        {
+            try
+            {
+                var Order = new Order();
+                Order.BuyerName = CustomerName;
+                Order.BoughtAt = DateTime.Now;
+                Order.CashierId = User.Identity.GetUserId();
+                Order.IsDeleted = false;
+                Order.TotalPrice = TotalPrice;
+                db.Orders.Add(Order);
+
+                db.SaveChanges();
+                foreach (var item in OrderDetails)
+                {
+                    OrderDetail detail = new OrderDetail();
+                    detail.OrderId = Order.Id;
+                    detail.ProductId = item.ProductId;
+                    detail.Quantity = item.Quantity;
+                    detail.Price = item.Price;
+
+                    var thisProd = db.Products.FirstOrDefault(s => s.Id == item.ProductId);
+                    thisProd.Quantity = thisProd.Quantity - item.Quantity;
+                    
+                    if(thisProd.Quantity < 10)
+                    {
+                        var newNotification = new Notification();
+                        newNotification.GeneratedOn = DateTime.Now;
+                        newNotification.LastUpdated = DateTime.Now;
+                        newNotification.NotificationDetail = "Only " + thisProd.Quantity + " is remaining in Stock of Product " + thisProd.ProductName;
+                        newNotification.StatusId = 1;
+                        db.Notifications.Add(newNotification);
+                    }
+
+                    db.OrderDetails.Add(detail);
+                    db.SaveChanges();
+                }
+                return Json("Success", JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json("Something went wrong", JsonRequestBehavior.AllowGet);
+            }
+
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DemoInven/Controllers/StocksController.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DemoInven.Models;
+using Microsoft.AspNet.Identity;
+
+namespace DemoInven.Controllers
+{
+    public class StocksController : Controller
+    {
+        private DemoInvenEntities db = new DemoInvenEntities();
+
+        // GET: Stocks
+        public ActionResult Index()
+        {
+            var stocks = db.Stocks.Include(s => s.AspNetUser).Include(s => s.Supplier);
+            return View(stocks.ToList());
+        }
+
+        // GET: Stocks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var stockDetails = db.StockDetails.Where(a=>a.StockId == id);
+            if (stockDetails == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stockDetails);
+        }
+
+        // GET: Stocks/Create
+        public ActionResult Create()
+        {
+            ViewBag.CreatedBy = new SelectList(db.AspNetUsers, "Id", "Email");
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "Id", "Name");
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "CategoryName");
+            return View();
+        }
+
+        // POST: Stocks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,StockName,StockDescription,CreatedBy,CreatedOn,IsDelete,SupplierId")] Stock stock)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Stocks.Add(stock);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CreatedBy = new SelectList(db.AspNetUsers, "Id", "Email", stock.CreatedBy);
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "Id", "Name", stock.SupplierId);
+            return View(stock);
+        }
+
+        // GET: Stocks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stock stock = db.Stocks.Find(id);
+            if (stock == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CreatedBy = new SelectList(db.AspNetUsers, "Id", "Email", stock.CreatedBy);
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "Id", "Name", stock.SupplierId);
+            return View(stock);
+        }
+
+        // POST: Stocks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,StockName,StockDescription,CreatedBy,CreatedOn,IsDelete,SupplierId")] Stock stock)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(stock).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CreatedBy = new SelectList(db.AspNetUsers, "Id", "Email", stock.CreatedBy);
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "Id", "Name", stock.SupplierId);
+            return View(stock);
+        }
+
+        // GET: Stocks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stock stock = db.Stocks.Find(id);
+            if (stock == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stock);
+        }
+
+        // POST: Stocks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Stock stock = db.Stocks.Find(id);
+            db.Stocks.Remove(stock);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        [HttpPost]
+        public JsonResult AddStock(Stock StockDetails, List<Product> ProductsDetails)
+        {
+            try
+            {
+                var stockObj = new Stock();
+                stockObj.StockName = StockDetails.StockName;
+                stockObj.StockDescription = StockDetails.StockDescription;
+                stockObj.CreatedBy = User.Identity.GetUserId();
+                stockObj.CreatedOn = DateTime.Now;
+                stockObj.IsDelete = false;
+                stockObj.SupplierId = StockDetails.SupplierId;
+                db.Stocks.Add(stockObj);
+                db.SaveChanges();
+                foreach (var item in ProductsDetails)
+                {
+                    if(item.Id == 0)
+                    {
+                        Product prodObj = new Product();
+                        prodObj.ProductName = item.ProductName;
+                        prodObj.IsDelete = false;
+                        prodObj.ProductDescription = item.ProductDescription;
+                        prodObj.Quantity = item.Quantity;
+                        prodObj.StockNo = stockObj.Id;
+                        prodObj.Price = item.Price;
+                        prodObj.CreatedBy = User.Identity.GetUserId();
+                        prodObj.CreatedOn = DateTime.Now;
+                        prodObj.CategoryId = item.CategoryId;
+                        prodObj.BarCode = null;
+                        db.Products.Add(prodObj);
+                        db.SaveChanges();
+
+                        StockDetail detailObj = new StockDetail();
+                        detailObj.ProductId = prodObj.Id;
+                        detailObj.QuantityAdded = item.Quantity;
+                        detailObj.StockId = stockObj.Id;
+                        db.StockDetails.Add(detailObj);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        Product prodObj = db.Products.FirstOrDefault(d=>d.Id == item.Id);
+                        prodObj.ProductName = item.ProductName;
+                        prodObj.IsDelete = false;
+                        prodObj.ProductDescription = item.ProductDescription;
+                        prodObj.Quantity = prodObj.Quantity + item.Quantity;
+                        prodObj.StockNo = stockObj.Id;
+                        prodObj.Price = item.Price;
+                        db.SaveChanges();
+
+                        StockDetail detailObj = new StockDetail();
+                        detailObj.ProductId = prodObj.Id;
+                        detailObj.QuantityAdded = item.Quantity;
+                        detailObj.StockId = stockObj.Id;
+                        db.StockDetails.Add(detailObj);
+                        db.SaveChanges();
+                        
+
+                    }
+
+                }
+
+
+                return Json("Success");
+            }
+            catch (Exception)
+            {
+                return Json("Something went wrong!");
+            }
+        }
+    }
+}
